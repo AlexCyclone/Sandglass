dccfc41333c94b310a7647180d2f63bbe5129c7c
AlexCyclone <aleksey.babanin@gmail.com>
2018-09-19 16:52:59 +0300

    o

--- src/icu/cyclone/alex/Main.java
+++ src/icu/cyclone/alex/Main.java
@@ -14,15 +14,19 @@ public class Main {
         if (size < 3 || size % 2 == 0) {
             throw new IllegalArgumentException("Incorrect input");
         }
+
         int iInc = 1;
         int jInc = -2;
+
         for (int i = 0, j = size; j <= size; i += iInc, j += jInc) {
             char[] spaces = new char[i];
             char[] stars = new char[j];
             Arrays.fill(spaces,' ');
             Arrays.fill(stars,'*');
+
             System.out.print(spaces);
             System.out.println(stars);
+
             if (j == 1) {
                 iInc = -1;
                 jInc = 2;
